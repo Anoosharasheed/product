0b8df87efbb6ad6c9183cb136602a7d6b6815fbd
Anoosharasheed <anooshar446@gmail.com>
2023-02-22 16:11:21 +0500

    products

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import './Productlist.css'
+
+function Navbar(){
+ return(
+    <div className="nav">
+        <div className="image">
+
+        </div>
+    </div>
+ )
+}
+export default Navbar;
---
+++ src/components/Productlist.jsx
@@ -0,0 +1,73 @@
+import './Productlist.css'
+
+function Productlist() {
+    return (
+
+        <div className='products'>
+        <div className='list1'>
+        <div class="image-box">
+        <img className='image1' src="https://raw.githubusercontent.com/jeff-lent/Alisnobba/main/Capstone/ActualRubyRubySlippers.jpg" />
+        </div>
+        <div class="about">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem maiores debitis eveniet nesciunt, perferendis, neque, dicta ex illo consequatur perspiciatis aliquid unde odio. Temporibus enim sint amet tempora eius modi?</p>
+        <h1 class="title">Ruby Slipper</h1>
+         <p class="subtitle">$12</p>
+        <button className='button'>Add to cart</button>
+        </div>
+
+            </div>
+            <div className='Cart2'>
+                <div class="image-box">
+                    <img className='image1' src="https://raw.githubusercontent.com/jeff-lent/Alisnobba/main/Capstone/ChocolatePudding.png" />
+                </div>
+                <div class="about">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem maiores debitis eveniet nesciunt, perferendis, neque, dicta ex illo consequatur perspiciatis aliquid unde odio. Temporibus enim sint amet tempora eius modi?</p>
+                    <h1 class="title">Ruby Slipper</h1>
+                    <p class="subtitle">$12</p>
+                    <button className='button'>Add to cart</button>
+                </div>
+
+            </div>
+            <div className='Cart3'>
+                <div class="image-box">
+                    <img className='image1' src="https://raw.githubusercontent.com/jeff-lent/Alisnobba/main/Capstone/DiamondWatch.jpg" />
+                </div>
+                <div class="about">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem maiores debitis eveniet nesciunt, perferendis, neque, dicta ex illo consequatur perspiciatis aliquid unde odio. Temporibus enim sint amet tempora eius modi?</p>
+                    <h1 class="title">Ruby Slipper</h1>
+                    <p class="subtitle">$12</p>
+                    <button className='button'>Add to cart</button>
+                </div>
+
+            </div>
+            <div className='Cart3'>
+                <div class="image-box">
+                    <img className='image1' src="https://raw.githubusercontent.com/jeff-lent/Alisnobba/main/Capstone/GoldenToilet.jpg" />
+                </div>
+                <div class="about">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem maiores debitis eveniet nesciunt, perferendis, neque, dicta ex illo consequatur perspiciatis aliquid unde odio. Temporibus enim sint amet tempora eius modi?</p>
+                    <h1 class="title">Ruby Slipper</h1>
+                    <p class="subtitle">$12</p>
+                    <button className='button'>Add to cart</button>
+                </div>
+
+            </div>
+            <div className='Cart3'>
+                <div class="image-box">
+                    <img className='image1' src="https://raw.githubusercontent.com/jeff-lent/Alisnobba/main/Capstone/LandYachtMotorHome.jpg"/>
+                </div>
+                <div class="about">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem maiores debitis eveniet nesciunt, perferendis, neque, dicta ex illo consequatur perspiciatis aliquid unde odio. Temporibus enim sint amet tempora eius modi?</p>
+                    <h1 class="title">Ruby Slipper</h1>
+                    <p class="subtitle">$12</p>
+                    <button className='button'>Add to cart</button>
+                </div>
+
+            </div>
+        </div>
+
+
+    )
+
+}
+export default Productlist;
